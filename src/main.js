9e0c99e62b14b7bb44c36054d87110de80d79089
HYHyper <huyuhang111@gmail.com>
2021-02-18 22:47:18 +0800

    '首页页面及效果'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+Vue.prototype.$bus = new Vue()
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
